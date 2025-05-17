e0452226e35c9e566e23e027fb6d2a808fa4f692
ramibelhadjsalem <ramibelhadjsalem06@gmail.com>
2025-05-17 04:07:46 +0100

    push

--- alter_chatbot_responses.sql
+++
@@ -1,8 +0,0 @@
--- Alter the chatbot_responses table to change the field name from 'patterns' to 'pattern'
-ALTER TABLE public.chatbot_responses 
-  RENAME COLUMN patterns TO pattern;
-
--- Verify the change
-SELECT column_name, data_type 
-FROM information_schema.columns 
-WHERE table_name = 'chatbot_responses' AND table_schema = 'public';
